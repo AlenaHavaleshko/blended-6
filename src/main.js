daa8ff3c0411e5ef00a3ac511d025e5e62d69d45
Mariana_k <m.koval000@gmail.com>
2025-05-23 21:59:39 +0300

    add task_2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import { getAllProducts } from "./requests/products";
-import { markupAllProducts } from "./services/markupService";
+import { getAllProducts, getProductById } from './requests/products';
+import { markupAllProducts, markupProduct } from './services/markupService';
 
 const allProducts = document.querySelector('#allProducts');
+const singleProductForm = document.querySelector('#singleProductForm');
+const singleProduct = document.querySelector('#singleProduct');
 
 // getAllProducts().then((console.log))
 
-getAllProducts().then(({data: {products}}) => {console.log(products)
- const markupCards = markupAllProducts(products);
- allProducts.insertAdjacentHTML('beforeend', markupCards);
+getAllProducts().then(({ data: { products } }) => {
+  console.log(products);
+  //  const markupCards = markupAllProducts(products);
+  //  allProducts.insertAdjacentHTML('beforeend', markupCards);
 });
 
-
-
+singleProductForm.addEventListener('submit', findProductById);
+function findProductById(event) {
+  event.preventDefault();
+  const id = event.target.elements.id.value;
+  getProductById(id).then(console.log);
+  getProductById(id).then(({ data }) => {
+    // console.log(data);
+    const markupSingleCard = markupProduct(data);
+    singleProduct.innerHTML = markupSingleCard;
+    singleProductForm.reset();
+  });
+}
